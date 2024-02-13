56d88fe1b58542a8f1d16235d14569de40f1b787
Augusto008 <aquila.real0@gmail.com>
2024-02-13 19:41:59 -0300

    UPDATE `Lesson2.vue`
    Add text and change title

--- src/components/lessons/Lesson2.vue
+++ src/components/lessons/Lesson2.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="lesson">
-    <h1>Tipos de Carne</h1>
+    <h1>Vestimenta e EPIs</h1>
+    <ul>
+      <li>Luvas anticorte</li>
+      <li>Óculos de proteção</li>
+      <li>Protetor auditivo</li>
+      <li>Luvas Térmicas</li>
+      <li>Calça frigorífica</li>
+      <li>Japona frigorífica</li>
+      <li>Respirador descartável</li>
+      <li>Avental</li>
+      <li>Bota de PVC</li>
+      <li>Perneiras</li>
+      <li>Mangotes</li>
+      <li>Touca descartável</li>
+    </ul>
+    <ul>
+      <li>uniforme branco</li>
+      <li>botas de borracha</li>
+      <li>touca</li>
+      <li>avental</li>
+      <li>perneiras</li>
+      <li>magotes</li>
+    </ul>
+    
   </div>
 </template>
 
