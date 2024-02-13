343d73361e2c68aceb8d89447e804cc625d40c61
Augusto008 <aquila.real0@gmail.com>
2024-02-13 20:11:28 -0300

    UPDATE `Lesson3.vue`
    Add text and change title

--- src/components/lessons/Lesson3.vue
+++ src/components/lessons/Lesson3.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="lesson">
-    <h1>Tipos de corte</h1>
+    <h1>Facas</h1>
+    <h2>Evolução das Facas de Açougueiro</h2>
+    <h3>Das ferramentas primitivas</h3>
+    <h3>Lâminas Especializadas</h3>
+    <h3>Facas Tradicionais vs Modernas</h3>
+    <h2>Tipos de Faca</h2>
+    <h3>Faca do Chef</h3>
+    <h3>Faca de Desossa</h3>
+    <h3>Faca para descascar (remover pele)</h3>
+    <h3>Cutelo</h3>
+    <h2>Escolhendo a melhor faca</h2>
+    <h3>Material da Lâmina</h3>
+    <h3>Comprimento da Lâmina</h3>
+    <h3>Design do Cabo</h3>
+    <h3>Equilíbrio e Peso</h3>
+    <h3>Manutenção e Durabilidade</h3>
   </div>
 </template>
 
