5e8f3b821fca8337b11ffe541c6db36fa0fb4b3f
Augusto008 <aquila.real0@gmail.com>
2024-02-13 20:13:40 -0300

    UPDATE `Lesson4.vue`
    Add text and meat types titles, add cut lists and meat info

--- src/components/lessons/Lesson4.vue
+++ src/components/lessons/Lesson4.vue
@@ -1,6 +1,161 @@
 <template>
   <div class="lesson">
-    <h1>Manipulação de Carnes</h1>
+    <h1>Tipos de Carne</h1>
+    <div>
+      <h3>Suína</h3>
+      <p>
+        Conhecida por sua versatilidade e sabor característico, é proveniente de porcos e inclui cortes como lombo, costela e pernil.<br>
+        É fonte de proteína animal mais consumida em todo o mundo, com sabor diferenciado e marcante.<br>
+        O Brasil é o 4º maior produtor e exportador de carne suína do mundo. O desempenho brasileiro é significativo quando comparado com a média mundial.
+      </p>
+      <p>
+        A carne suína é muito versátil, nutritiva e pode agradas aos mais diferentes paladares. São cortes macios, saborosos e suculentos, prontos para grelhar, cozinhar ou assar. Com ela, é possível preparar pratos práticos e muito deliciosos: de um rápido sanduíche, como refeições diárias, até receitas mais elaboradas para surpreender a todos.<br>
+        O seu preparo pode ser feito com temperos tradicionais e ou mais elaborados, como as combinações de ingredientes que resultam em sabores cítricos e agridoces.<br>
+        Pensar nos acompanhamentos é mais simples do que se imagina. Combinam muito bem com legumes, farofas, massas e vai muito bem com o básico e tradicional arroz com feijão.<br>
+        Além de equilibrar sabor, praticidade, qualidade e custo-benefício, a carne suína ainda oferece uma extensa variedade de cortes.
+      </p>
+      <ol class="d-flex flex-column text-justify">
+        <li><strong>Alcatra</strong>: </li>
+        <li><strong>Barriga</strong>: </li>
+        <li><strong>Bisteca</strong>: </li>
+        <li><strong>Carré</strong>: </li>
+        <li><strong>Copa Lombo</strong>: </li>
+        <li><strong>Costela</strong>: </li>
+        <li><strong>Costela do Dianteiro</strong>: </li>
+        <li><strong>Coxão Duro</strong>: </li>
+        <li><strong>Coxão Mole</strong>: </li>
+        <li><strong>Fraldinha</strong>: </li>
+        <li><strong>Filé Mignon</strong>: </li>
+        <li><strong>Joelho</strong>: </li>
+        <li><strong>Lombo</strong>: </li>
+        <li><strong>Lombo Inglês</strong>: </li>
+        <li><strong>Maminha</strong>: </li>
+        <li><strong>Orelha</strong>: </li>
+        <li><strong>Paleta / Shoulder / Miolo da Paleta</strong>: </li>
+        <li><strong>Panceta</strong>: </li>
+        <li><strong>Papada</strong>: </li>
+        <li><strong>Patinho</strong>: </li>
+        <li><strong>Pé</strong>: </li>
+        <li><strong>Pernil</strong>: </li>
+        <li><strong>Picanha</strong>: </li>
+        <li><strong>Prime Rib / Lombo Tomahawk</strong>: </li>
+        <li><strong>Ossobuco</strong>: </li>
+        <li><strong>Rabo</strong>: </li>
+        <li><strong>Ribee / Ribeye</strong>: </li>
+        <li><strong>Suã</strong>: </li>
+        <li><strong>Stinco</strong>: </li>
+        <li><strong>Sobrepaleta / Short Ribe</strong>: </li>
+        <li><strong>Tatú</strong>: </li>
+        <li><strong>T-bone</strong>: </li>
+      </ol>
+    </div>
+    <div>
+      <h3>Bovina</h3>
+      <p>
+        Sua qualidade pode variar de acordo com a idade do animal, a raça e a alimentação. É proveniente de bovinos como vacas e bois.<br>
+        A carne bovina é a mais consumida no mundo. Ela está presente na mesa e no gosto dos consumidores brasileiros e, principalmente, o agronegócios do país.<br>
+        Contudo, são poucos aqueles que, de fato, sabem identificar e diferenciar os diversos tipos de cortes de carne bovina.<br>
+      </p>
+      <p>
+        Cada corte de carne de boi tem suas características peculiares, que se adequam a todos os gostos e orçamentos.
+      </p>
+      <ol>
+        <li><strong>Aba do filé</strong>: </li>
+        <li><strong>Acém</strong>: </li>
+        <li><strong>Alcatra</strong>: </li>
+        <li><strong>Ancho</strong>: </li>
+        <li><strong>Assado em Tiras</strong>: </li>
+        <li><strong>Bife do Vazio</strong>: </li>
+        <li><strong>Capa de filé</strong>: </li>
+        <li><strong>Chorizo</strong>: </li>
+        <li><strong>Contra filé</strong>: </li>
+        <li><strong>Costela Desoçada</strong>: </li>
+        <li><strong>Costela Janela</strong>: </li>
+        <li><strong>Costela Minga</strong>: </li>
+        <li><strong>Costela Três Ripas</strong>: </li>
+        <li><strong>Coxão duro</strong>: </li>
+        <li><strong>Coxão mole</strong>: </li>
+        <li><strong>Cupim</strong>: </li>
+        <li><strong>Denver Steck</strong>: </li>
+        <li><strong>Entrecôte</strong>: </li>
+        <li><strong>Fraldinha</strong>: </li>
+        <li><strong>Filé de Costela</strong>: </li>
+        <li><strong>Filé mignon</strong>: </li>
+        <li><strong>Lagarto</strong>: </li>
+        <li><strong>Maminha</strong>: </li>
+        <li><strong>Maminha da alcatra</strong>: </li>
+        <li><strong>Miolo do Acém</strong>: </li>
+        <li><strong>Músculo dianteiro</strong>: </li>
+        <li><strong>Músculo traseiro</strong>: </li>
+        <li><strong>Patinho</strong>: </li>
+        <li><strong>Paleta ou Miolo de Paleta</strong>: </li>
+        <li><strong>Paleta Porcionada</strong>: </li>
+        <li><strong>Paleta sem Músculo</strong>: </li>
+        <li><strong>Paleta Sete</strong>: </li>
+        <li><strong>Patinho</strong>: </li>
+        <li><strong>Peito</strong>: </li>
+        <li><strong>Pescoço</strong>: </li>
+        <li><strong>Picanha</strong>: </li>
+        <li><strong>Picanha Fatiada</strong>: </li>
+        <li><strong>Ponta de agulha</strong>: </li>
+        <li><strong>Ponta de Peito</strong>: </li>
+        <li><strong>Prime Rib</strong>: </li>
+        <li><strong>Rabo</strong>: </li>
+        <li><strong>Short Rib</strong>: </li>
+        <li><strong>Tomahawk</strong>: </li>
+        <li><strong>Tatu</strong>: </li>
+
+      </ol>
+    </div>
+    <div>
+      <h3>Ovina</h3>
+      <p>
+        
+      </p>
+      <ol>
+        <li>Pescoço</li>
+        <li>Paleta</li>
+        <li>Carré</li>
+        <li>T-bone</li>
+        <li>Costela</li>
+        <li>Costela Vazio</li>
+        <li>Picanha</li>
+        <li>Pernil</li>
+      </ol>
+    </div>
+    <div>
+      <h3>Aves(Frango)</h3>
+      <p></p>
+    </div>
+    <div>
+      <h3>Caprina</h3>
+      <p></p>
+    </div>
+    <div>
+      <h3>Turquia</h3>
+      <p></p>
+    </div>
+    <div>
+      <h3>Faisão</h3>
+      <p></p>
+    </div>
+    <div>
+      <h3>Pato</h3>
+      <p></p>
+    </div>
+    <div>
+      <h3>Coelho</h3>
+      <p></p>
+    </div>
+    <div>
+      <h3>Frutos do mar</h3>
+      <p></p>
+    </div>
+    <div>
+      <h3>Caça</h3>
+      <p></p>
+    </div>
+
   </div>
 </template>
 
